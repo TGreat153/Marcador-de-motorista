5688be30f7888f2a0baa1723538c97495873c7b8
TGreat153 <tarciodesantana2013@gmail.com>
2026-05-18 13:12:58 -0300

    add home and index

---
+++ marcador-motorista/src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './home/Home';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <>
+        <div id='header'>
+          <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="/">
+                Marcador de motoristas
+              </a>
+            </div>
+          </nav>        
+        </div>
+        <div id='body'>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+          </Routes>
+        </div>
+        <div id='footer'>
+          <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+            <div className="container-fluid d-flex justify-content-between">
+              <span className="navbar-brand mb-0 h1">Empresa Legal</span>
+              <span className="text-light">Telefone: (81) 99999-9999</span>
+              <span className="text-light">E-mail: contato@empresa.com</span>
+            </div>
+          </nav>
+        </div>
+      </>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ marcador-motorista/src/home/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+
+  return (
+    <>
+        <h1>Olá, deseja marcar uma viagem?</h1>
+        <button type="button" class="btn btn-primary">Marcar motorista</button>
+    </>
+  )
+}
+
+export default Home
